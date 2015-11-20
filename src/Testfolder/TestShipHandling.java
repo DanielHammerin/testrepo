dc6f4ecb9df984af7090e449b05e49acd21a2210
DanielHammerin <danne16>
2015-11-20 10:32:35 +0100

    *****RED***** added testclass testShipHandling. added testmethod testAddShipReturnsTrue()

---
+++ src/Testfolder/TestShipHandling.java
@@ -0,0 +1,23 @@
+package Testfolder;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by Daniel on 2015-11-19.
+ */
+public class TestShipHandling extends TestCase{
+
+    ShipHandling shipHandling = new ShipHandling();
+
+    @Test
+    public void testAddShipReturnsTrue() throws Exception {
+        String shipName = "Victorium";
+        String shipClass = "Battleship";
+        int shipGunCaliber = 305;
+        int shipLength = 320;
+        int shipNGuns = 10;
+
+        assertTrue(shipHandling.addShip(shipName, shipClass, shipGunCaliber, shipLength, shipNGuns));
+    }
+}
