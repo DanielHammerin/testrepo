b7c3a73648098c98f920656a448a6c9df27b3dc9
DanielHammerin <danne16>
2015-11-20 11:37:59 +0100

    *****RED***** addded testChangeShipReturnsTrue()

--- src/Testfolder/TestShipHandling.java
+++ src/Testfolder/TestShipHandling.java
@@ -1,6 +1,8 @@
 package Testfolder;
 
 import Programfolder.Member;
+import Programfolder.SQLDUMMY;
+import Programfolder.Ship;
 import Programfolder.ShipHandling;
 import junit.framework.TestCase;
 import org.junit.Test;
@@ -14,7 +16,7 @@ public class TestShipHandling extends TestCase{
 
     @Test
     public void testAddShipReturnsTrue() throws Exception {
-        Member tempMem = new Member();
+        Member tempMem = new Member();                  //Owner
         tempMem.setMemberFirstName("Maximum");
         tempMem.setMemberLastName("Crispness");
         tempMem.setMemberID("MC999");
@@ -26,5 +28,33 @@ public class TestShipHandling extends TestCase{
         int shipNGuns = 10;
 
         assertTrue(shipHandling.addShip(tempMem, shipName, shipClass, shipGunCaliber, shipLength, shipNGuns));
+    }
+
+    @Test
+    public void testChangeShipReturnsTrue() throws Exception {
+        Member tempMem = new Member();              //Temp owner
+        tempMem.setMemberFirstName("Maximum");
+        tempMem.setMemberLastName("Crispness");
+        tempMem.setMemberID("MC999");
+
+        String shipName = "Victorium";
+        String shipClass = "Battleship";
+        int shipGunCaliber = 305;
+        int shipLength = 320;
+        int shipNGuns = 10;
+
+        assertTrue(shipHandling.addShip(tempMem, shipName, shipClass, shipGunCaliber, shipLength, shipNGuns)); //Add ship
+
+        Ship tempShip = new Ship();                 //Create instance of ship to be changed.
+        for (Ship s : SQLDUMMY.getAllShips()) {
+            tempShip = s;                           //Only one ship in list.
+        }
+        String newShipName = "Leopleurodon";
+        String newShipClass = "Cruiser";
+        int newShipGunCaliber = 152;
+        int newShipLength = 200;
+        int newShipNGuns = 16;
+
+        assertTrue(shipHandling.changeShip(tempShip, newShipName, newShipClass, newShipGunCaliber, newShipLength, newShipNGuns));   //Change the ship fields.
     }
 }
