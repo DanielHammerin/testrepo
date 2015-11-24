aaf136e5078c9ae75313aaee774d05af72900a3b
DanielHammerin <danne16>
2015-11-24 01:42:46 +0100

    *****REFRACTOR***** implemented displayHeader()

--- src/Programfolder/View.java
+++ src/Programfolder/View.java
@@ -6,6 +6,12 @@ package Programfolder;
 public class View {
 
     public void displayheader() {
-
+        System.out.println("Welcome to battleships. This service lets you register your battleships.");
+        System.out.println("Press 'NC' to add a new captain.");
+        System.out.println("Press 'NS' to add a new ship to a captain's reserve.");
+        System.out.println("Press 'CC' to change a captain's name.");
+        System.out.println("Press 'CS' to change a ship's data.");
+        System.out.println("Press 'DC' to delete a captain.");
+        System.out.println("Press 'DS' to delete a ship.");
     }
 }
