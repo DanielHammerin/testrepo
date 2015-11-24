7980dd62769e1058a9ec2b2cd90c49d1046928b0
DanielHammerin <danne16>
2015-11-24 01:48:12 +0100

    *****RED***** added test class "TestController" and testCommandControll()

---
+++ src/Testfolder/TestController.java
@@ -0,0 +1,32 @@
+package Testfolder;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Daniel on 2015-11-24.
+ */
+public class TestController {
+
+    private Controller ctrl;
+
+    @Before
+    public void setUp() {
+        ctrl = mock(Controller.class);
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testCommandControll() {
+        ctrl.commandControll();
+        verify(ctrl, Mockito.times(1)).commandControll();
+    }
+}
